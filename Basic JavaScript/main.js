1fc9d41e71f8d8bb170ab446938f19f97dbf34bb
Sara Alkhamri <s.alkhamri@gmail.com>
2020-02-27 13:49:59 -0800

    Second assignment completed

--- Basic JavaScript/main.js
+++ Basic JavaScript/main.js
@@ -27,3 +27,24 @@ function processArg(num) {
 
 // Only change code below this line
 processed = processArg(7);
+
+{/* <---------------------------------------> */}
+
+//2 Stand in Line
+// In Computer Science a queue is an abstract Data Structure where items are kept in order. New items can be added at the back of the queue and old items are taken off from the front of the queue.
+
+// Write a function nextInLine which takes an array (arr) and a number (item) as arguments.
+
+// Add the number to the end of the array, then remove the first element of the array.
+
+// The nextInLine function should then return the element that was removed.
+
+function nextInLine(arr, item) {
+    // Your code here
+    arr.push(item) //push item to end or array
+    var removed = arr.shift() //remove first item in array
+    return removed;  // Change this line
+  }
+  
+  
+  
