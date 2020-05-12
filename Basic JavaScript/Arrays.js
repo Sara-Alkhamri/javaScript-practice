8d8cb08e8ecf81807b8d6fe8b244e2145b519111
Sara Alkhamri <s.alkhamri@gmail.com>
2020-05-11 21:02:47 -0700

    parseInt Function quiz done

--- Basic JavaScript/Arrays.js
+++ Basic JavaScript/Arrays.js
@@ -492,3 +492,21 @@ function randomWholeNum() {
 function randomRange(myMin, myMax) {
   return Math.floor(Math.random() * (myMax - myMin + 1) + myMin);
 }
+
+// Use the parseInt Function
+// The parseInt() function parses a string and returns an integer. Here's an example:
+
+// var a = parseInt("007");
+
+// The above function converts the string "007" to an integer 7. If the first character in the string can't be converted into a number, then it returns NaN.
+
+// Use parseInt() in the convertToInteger function so it converts the input string str into an integer, and returns it.
+
+//Solution:
+
+function convertToInteger(str) {
+  return parseInt(str)
+}
+
+convertToInteger("56");
+
