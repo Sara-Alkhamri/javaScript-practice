2a9582c8cd71887359eef84bb3266beb96d2bc01
Sara Alkhamri <s.alkhamri@gmail.com>
2020-05-21 22:05:30 -0700

    Destructuring Assignment to Pass an Object as a Functions Parameters

--- Basic JavaScript/ES6.js
+++ Basic JavaScript/ES6.js
@@ -433,3 +433,38 @@ function removeFirstTwo(list) {
   return arr;
 }
 const arr = removeFirstTwo(source);
+
+
+// Use Destructuring Assignment to Pass an Object as a Function's Parameters
+// In some cases, you can destructure the object in a function argument itself.
+
+// Consider the code below:
+
+// const profileUpdate = (profileData) => {
+//   const { name, age, nationality, location } = profileData;
+//   // do something with these variables
+// }
+// This effectively destructures the object sent into the function. This can also be done in-place:
+
+// const profileUpdate = ({ name, age, nationality, location }) => {
+//   /* do something with these fields */
+// }
+// This removes some extra lines and makes our code look neat. This has the added benefit of not having to manipulate an entire object in a function — only the fields that are needed are copied inside the function.
+
+// Use destructuring assignment within the argument to the function half to send only max and min inside the function.
+
+//Solution:
+
+const stats = {
+  max: 56.78,
+  standard_deviation: 4.34,
+  median: 34.54,
+  mode: 23.87,
+  min: -0.75,
+  average: 35.85
+};
+
+// Only change code below this line
+const half = ({ max, min }) => (max + min) / 2.0;
+// Notice that we are destructuring stats to pass two of its attributes - max and min - to the function. Don’t forget to the modify the second return statement.
+// Change stats.max to just max, and change stats.min to just min.
