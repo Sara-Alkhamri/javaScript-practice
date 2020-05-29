455f28daaef1bd82aa3fae7b7c9381cfee247f62
Sara Alkhamri <s.alkhamri@gmail.com>
2020-05-29 16:26:09 -0700

    Extract Matches done

--- Basic JavaScript/RegularExpressions.js
+++ Basic JavaScript/RegularExpressions.js
@@ -77,3 +77,29 @@ let result = petRegex.test(petString);
 let myString = "freeCodeCamp";
 let fccRegex = /freeCodeCamp/i; // Change this line
 let result = fccRegex.test(myString);
+
+
+// Extract Matches
+// So far, you have only been checking if a pattern exists or not within a string. You can also extract the actual matches you found with the .match() method.
+
+// To use the .match() method, apply the method on a string and pass in the regex inside the parentheses.
+
+// Here's an example:
+
+// "Hello, World!".match(/Hello/);
+// // Returns ["Hello"]
+// let ourStr = "Regular expressions";
+// let ourRegex = /expressions/;
+// ourStr.match(ourRegex);
+// // Returns ["expressions"]
+// Note that the .match syntax is the "opposite" of the .test method you have been using thus far:
+
+// 'string'.match(/regex/);
+// /regex/.test('string');
+// Apply the .match() method to extract the word coding.
+
+//Solution:
+
+let extractStr = "Extract the word 'coding' from this string.";
+let codingRegex = /coding/; // Change this line
+let result = extractStr.match(/coding/); // Change this line
