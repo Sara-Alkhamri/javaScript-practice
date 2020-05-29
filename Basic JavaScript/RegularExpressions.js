6dacdae664154e064bfcdcd05affbefac082d21c
Sara Alkhamri <s.alkhamri@gmail.com>
2020-05-29 15:34:02 -0700

    Added notes to regularExpressions.js

---
+++ Basic JavaScript/RegularExpressions.js
@@ -0,0 +1,4 @@
+//Regular expressions are special strings that represent a search pattern.
+//Also known as "regex" or "regexp", they help programmers match, search, and replace text. 
+//Regular expressions can appear cryptic because a few characters have special meaning. 
+//The goal is to combine the symbols and text into a pattern that matches what you want, but only what you want.
